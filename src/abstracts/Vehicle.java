711d73d4cec9794b3f32a867c8bb55f8253a4456
Nahom <nahumtorba@gmail.com>
2025-07-20 14:20:18 +0300

    Create abstract class Vehicle and two intefaces

--- src/abstracts/Vehicle.java
+++ src/abstracts/Vehicle.java
@@ -1,5 +1,37 @@
 package abstracts;
 
-public class Vehicle {
+public abstract class Vehicle {
+    private String vehicleId;
+    private String model;
+    private boolean isAvailable;
 
+    public Vehicle(String vehicleId, String model, boolean isAvailable) {
+        this.vehicleId = vehicleId;
+        this.model = model;
+        this.isAvailable = true;
+    }
+    //getters
+    public String getVehicleId() {
+        return vehicleId;
+    }
+    public String getModel() {
+        return model;
+    }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    //setter for available other fields are immutable
+    protected void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    //methods
+    public abstract void rentOut();
+    public abstract void returnVehicle();
+
+    @Override
+    public String toString() {
+        return vehicleId + " - " + model + " - " + (isAvailable ? "available" : "not available");
+    }
 }
