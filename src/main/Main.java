149783a387282139cabd2348931c6a087a6d60f5
Nahom <nahumtorba@gmail.com>
2025-08-01 22:34:28 +0300

    edit main class to run main menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,22 @@
 package main;
 
+import main.java.project.ui.MainMenu;
 import main.java.project.dao.DBConnection;
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
         try (Connection conn = DBConnection.getConnection()) {
-            System.out.println("Connected to database!");
+            System.out.println("Connected to database successfully!");
+
+            // Start the Car Rental Management System
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.start();
+
         } catch (Exception e) {
+            System.err.println("Failed to connect to database: " + e.getMessage());
             e.printStackTrace();
+            System.exit(1);
         }
     }
 }
